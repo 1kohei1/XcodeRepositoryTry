6defd6e9dacc48cd2ed029966f04d3b6bc93f95c
Kohei Arai <tobecomebig@gmail.com>
2014-08-21 15:15:16 -0400

    Initial Commit

---
+++ XcodeRepositoryTry/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  XcodeRepositoryTry
+//
+//  Created by Kohei Arai on 8/21/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ XcodeRepositoryTry/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  XcodeRepositoryTry
+//
+//  Created by Kohei Arai on 8/21/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
