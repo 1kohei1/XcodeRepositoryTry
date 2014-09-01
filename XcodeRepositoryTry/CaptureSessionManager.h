02277d46e3cc0848907e3e73121f7870e8166d96
Kohei Arai <tobecomebig@gmail.com>
2014-09-01 10:19:56 -0400

    Initiate: Overlaying image

---
+++ XcodeRepositoryTry/CaptureSessionManager.h
@@ -0,0 +1,16 @@
+//
+//  CaptureSessionManager.h
+//  XcodeRepositoryTry
+//
+//  Created by Kohei Arai on 9/1/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <AVFoundation/AVFoundation.h>
+
+@interface CaptureSessionManager : NSObject {
+
+}
+
+@end
---
+++ XcodeRepositoryTry/CaptureSessionManager.m
@@ -0,0 +1,19 @@
+//
+//  CaptureSessionManager.m
+//  XcodeRepositoryTry
+//
+//  Created by Kohei Arai on 9/1/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import "CaptureSessionManager.h"
+
+@implementation CaptureSessionManager
+
+#pragma mark Capture Session Configuration
+
+-(id) init {
+
+}
+
+@end
