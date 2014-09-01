257ba256573bf789ffb6bd648e0e6f18945bc1f1
Kohei Arai <tobecomebig@gmail.com>
2014-09-01 10:32:35 -0400

    Initiate: AROverlayViewController class

---
+++ XcodeRepositoryTry/AROverlayViewController.h
@@ -0,0 +1,14 @@
+//
+//  AROverlayViewController.h
+//  XcodeRepositoryTry
+//
+//  Created by Kohei Arai on 9/1/14.
+//  Copyright (c) 2014 Kohei Arai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AROverlayViewController : UIViewController
+
+
+@end
