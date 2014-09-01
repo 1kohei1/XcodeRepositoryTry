a6cf2326d27adc534bb631c8fc8ad60e4704bd27
Kohei Arai <tobecomebig@gmail.com>
2014-09-01 11:03:12 -0400

    Implement: AROverlayViewController class

--- XcodeRepositoryTry/AROverlayViewController.m
+++ XcodeRepositoryTry/AROverlayViewController.m
@@ -26,7 +26,44 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    // Set up capture manager
+    [self setCaptureManager:[[CaptureSessionManager alloc] init]];
+    [[self captureManager] addVIdeoInput];
+    
+    [[self captureManager] addVideoPreviewLayer];
+    CGRect layerRect = [[[self view] layer]bounds];
+    [[[self captureManager] previewLayer] setBounds:layerRect];
+    [[[self captureManager] previewLayer] setPosition:CGPointMake(CGRectGetMidX(layerRect), CGRectGetMidY(layerRect))];
+    [[[self view] layer] addSublayer:[[self captureManager] previewLayer]];
+    
+    // Set up overlay image
+    UIImageView *overlayImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"overlaygraphic.png"]];
+    [overlayImageView setFrame:CGRectMake(30, 100, 260, 200)];
+    [[self view] addSubview:overlayImageView];
+    
+    // Set up overlay buttons
+    UIButton *overlayButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    [overlayButton setImage:[UIImage imageNamed:@"scanbutton.png"] forState:UIControlStateNormal];
+    [overlayButton setFrame:CGRectMake(130, 320, 60, 30)];
+    [overlayButton addTarget:self action:@selector(scanButtonPressed) forControlEvents:UIControlEventTouchUpInside];
+    [[self view] addSubview:overlayButton];
+    
+    // Set up overlay scannning
+    UILabel *tempLabel = [[UILabel alloc] initWithFrame:CGRectMake(100, 50, 120, 30)];
+    [tempLabel setBackgroundColor:[UIColor clearColor]];
+    [tempLabel setFont:[UIFont fontWithName:@"Courier" size:18.0]];
+    [tempLabel setTextColor:[UIColor redColor]];
+    [tempLabel setText: @"Scanning..."];
+    [tempLabel setHidden:YES];
+    [self setScanningLabel:tempLabel];
+    [[self view] addSubview:[self scanningLabel]];
+    
+    [[[self captureManager] captureSession] startRunning];
+}
+
+- (void)scanButtonPressed {
+    
 }
 
 - (void)didReceiveMemoryWarning
